25923b4276cb1eb7b19ea17f5fdf15f1ca18e3e3
Joshua Ryan <admin@joshua-ryan.ca>
2020-03-27 15:00:58 -0400

    add functions to revert state, based on history

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 //array to hold previous app state(s) html
 let history = [];
+//counter to check against history, increases with each click
+let historyCount = 0;
 
 //variable to hold fetched chart data from data.json
 let charts;
@@ -16,10 +18,27 @@ function fetchData(){
     })
 }
 
+//add html to history and increase history counter
+function forwardHistory(){
+    history.push(document.querySelector("#interactive").innerHTML);
+    historyCount++;
+}
+
+//add html to history and decrease history counter
+function backHistory(){
+    if(historyCount.length > 1){
+        history.pop();
+        historyCount--;
+        document.querySelector("#interactive").innerHTML = history[historyCount];    
+    }
+}
+
+//load initial selections to page
 function initialAdd(){
     for(let i = 0; i < charts.length; i++){
         document.querySelector("#interactive").innerHTML += `<button data-id="${i}">${charts[i].name}</button>`
     }
+    forwardHistory();
 }
 
 //sequence events
