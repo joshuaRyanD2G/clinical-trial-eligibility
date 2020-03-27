0d015156434aa3d92e853e116a6fc80e0c663f91
Joshua Ryan <admin@joshua-ryan.ca>
2020-03-27 14:20:23 -0400

    add function to fetch data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-//main script
+//global variables
+
+//array to hold previous app state(s) html
+let history = [];
+
+//variable to hold fetched chart data from data.json
+let charts;
+
+//fetch the data
+function fetchData(){
+    fetch('./data/data.json')
+    .then(data => data.json())
+    .then(data => {
+        charts = data.charts;
+        console.log(charts);
+    })
+}
+//sequence events
+function init(){
+    fetchData();
+}
+
+//initialize
+document.addEventListener("DOMContentLoaded", function(){
+    init();
+});
